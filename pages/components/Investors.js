4a01101326bea2d567d0968b7baa6b8213a59cf5
anviiiijain <anvijain250102@gmail.com>
2021-05-08 15:42:20 +0530

    INVESTORS SECTION

---
+++ pages/components/Investors.js
@@ -0,0 +1,21 @@
+const Investors=()=>{
+    return(
+        <div>
+            <div className="mx-auto text-center w-3/12 "> 
+                <div className="text-sm font-semibold text-gray-600">Well Trusted</div>
+                <div className="text-2xl font-bold">Meet some of our well trusted Investors</div>
+            </div>
+            <div className="grid grid-cols-3 mx-auto w-2/5 items-center my-12">
+                <img className="w-36" src="./invest1.png"/>
+                <img className="w-36" src="./invest2.png"/>
+                <img className="w-36" src="./invest3.png"/>
+                <img className="w-36" src="./invest4.png"/>
+                <img className="w-36" src="./invest5.png"/>
+                <img className="w-36" src="./invest6.png"/>
+
+            </div>
+        </div>
+    )
+}
+
+export default Investors
