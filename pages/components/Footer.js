acca57d27d3647998c61d94114089639d991e1d4
anviiiijain <anvijain250102@gmail.com>
2021-05-08 15:40:41 +0530

    FOOTER

---
+++ pages/components/Footer.js
@@ -0,0 +1,45 @@
+const Footer=()=>{
+    return(
+        <div className="bg-smoke">
+            <div className="grid grid-cols-4">
+            
+                <div>
+                    <div className="font-bold text-lg">DoNotPay</div>
+                    <div>The DoNotPay app is the home of the world's robot lawyer.fight corporations , beat bureaucracy and sue anyone of the press of a button</div>
+                    <div>Made in California</div>
+                    <div>San Francisco</div>
+                </div>
+                
+                <div>
+                    <div className="font-bold text-sm">QUICK LINK</div>
+                    <div>
+                        <div>Overview</div>
+                        <div>Invite Friends</div>
+                        <div>Contact Us</div>
+                    </div>
+                </div>
+                <div>
+                    <div className="font-bold text-sm">LEGAL</div>
+                    <div>
+                        <div>Terms of use</div>
+                        <div>Privacy Policy</div>
+                        <div>Report a problem</div>
+                    </div>
+                </div>
+                <div>
+                    <div className="font-bold text-sm">CONTACT</div>
+                    <div>
+                        <div>180 Suite 201</div>
+                        <div>California</div>
+                        <div>support@donotpay.com</div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>©2020 donotpay | All rights reserved</div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
