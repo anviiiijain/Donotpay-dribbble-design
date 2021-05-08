e030f243d8f98afe08391ca0108a5479380fc7ff
anviiiijain <anvijain250102@gmail.com>
2021-05-08 15:39:17 +0530

    HERO SECTION

---
+++ pages/components/Hero.js
@@ -0,0 +1,17 @@
+const Hero=()=>{
+    return(
+        <div className="flex justify-evenly bg-blue-200 rounded-bl-3xl px-36 py-20 bg-gradient-to-tr from-violet  via-pink to-red-500">
+            <div className="text-white flex-1 px-32">
+                <div className="font-bold text-5xl leading-normal">Work  where you love.</div>
+                <div className="text-sm leading-relaxed py-5 pr-24">Want to join an exciting,dynamic team working on the legal side of things inside tech?</div>
+                <div className="text-xs bg-pink px-4 py-2 my-3 w-24 rounded">Find a Role</div>
+            </div>
+            <div className="flex-1">
+                <div><img/></div>
+                <div><img/></div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange' : '#f6f9fc',
+        'pink' : '#e13a8a',
+        'violet' : '#871df7',
+        'smoke' : '#f6f9fc',
+        'purple': {
+          '50' : '#d54cf4',
+          '100' : '#9b35d0',
+          '200': '#bb38c1',
+      }
+      }
+    },
   },
   variants: {
     extend: {},
